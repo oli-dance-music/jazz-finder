28d10a0bdde9bf6d057d812eb56f468ef1db7e11
oli-dance-music <oli.dance.music@gmail.com>
2024-04-03 16:24:06 +0200

    tiny text change

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		<h1 style={{ textAlign: 'center' }}>Jazz Finder</h1>
 		<JazzFinder />
 		<div style={{ textAlign: 'center', fontSize: '3rem', fontWeight: 'bold' }}>
-			...more coming soon...
+			...more features coming soon...
 		</div>
 	</React.StrictMode>
 );
